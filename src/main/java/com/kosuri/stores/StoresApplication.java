fa3eb671eefecd0d714da88b485b389f891b0878
= <=>
2023-03-28 11:02:47 +0530

    Initial commit

---
+++ src/main/java/com/kosuri/stores/StoresApplication.java
@@ -0,0 +1,14 @@
+package com.kosuri.stores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoresApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StoresApplication.class, args);
+		System.out.println("Hello World");
+	}
+}
