acd58f9b3f00684d9760a95777cca98a0f19fceb
nikita <nikitagupta5566@gmail.com>
2023-04-17 17:30:21 +0530

    Fix cors issue

--- src/main/java/com/kosuri/stores/StoresApplication.java
+++ src/main/java/com/kosuri/stores/StoresApplication.java
@@ -3,8 +3,11 @@ package com.kosuri.stores;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.kosuri.stores.*")
@@ -16,5 +19,15 @@ public class StoresApplication {
 		SpringApplication.run(StoresApplication.class, args);
 		System.out.println("Hello World");
 		System.out.println("Checking if push to git works");
+	}
+
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+		};
 	}
 }
